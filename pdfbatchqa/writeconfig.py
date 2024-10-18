d6e6ac94a1396cdc22e5bdc39ce3265d49518b18
bitsgalore <johan.vanderknijff@kb.nl>
2024-10-18 20:47:20 +0200

    removed obsolete stuff from config file module

--- pdfbatchqa/writeconfig.py
+++ pdfbatchqa/writeconfig.py
@@ -4,7 +4,6 @@
 """
 
 import os
-import platform
 
 def writeConfigFile(configFile):
     """Create default configuration file"""
@@ -19,26 +18,12 @@ def writeConfigFile(configFile):
     # Path to configuration file
     fConfig = os.path.join(configDir, fName)
 
-    # Generate string with config settings. This is platform-dependent,
-    # so first get the platform
-    myPlatform = platform.system()
+    # Generate string with config settings.
 
-    if myPlatform == "Windows":
-        # Paths will need manual editing, because this is not standardized
-        configString = '# settings pdfbatchqa\n'
-        configString += 'profile = dbnl-fulltext.xml\n'
-        configString += 'prefix = pdfbatchqa\n'
-        configString += 'pdfimages = C:\\poppler-24.07.0\\Library\\bin\\pdfimages.exe # pdfimages executable # pdfimages executable\n'
-        configString += 'pdfinfo = C:\\poppler-24.07.0\\Library\\bin\\pdfinfo.exe # pdfinfo executable\n'
-        configString += 'exiftool = C:\\exiftool\\exiftool.exe # exiftool executable\n'
-    else:
-        # This works on Linux, and probably MacOS as well
-        configString = '# settings pdfbatchqa\n'
-        configString += 'profile = dbnl-fulltext.xml\n'
-        configString += 'prefix = pdfbatchqa\n'
-        configString += 'pdfimages = pdfimages # pdfimages executable\n'
-        configString += 'pdfinfo = pdfinfo # pdfinfo executable\n'
-        configString += 'exiftool = exiftool # exiftool executable\n'
+    # Paths will need manual editing, because this is not standardized
+    configString = '# settings pdfbatchqa\n'
+    configString += 'profile = dbnl-fulltext.xml\n'
+    configString += 'prefix = pdfbatchqa\n'
 
     # Write string to configuration file
     with open(fConfig,"wb") as f:
