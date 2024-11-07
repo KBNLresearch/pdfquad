215a0b12bdc2f998d9165b6f790d78ce7ad76904
bitsgalore <johan.vanderknijff@kb.nl>
2024-11-07 19:54:01 +0100

    removed test script

--- pdfquad/test-jp2.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image, ImageEnhance, ImageOps
-
-#imageIn = "/home/johan/kb/digitalisering/dbnl/pdfs-2023/PDFs-scans/images-BKT/ecur-000.jpg"
-imageIn = "/home/johan/kb/digitalisering/dbnl/pdfs-2023/PDFs-scans/images-scanproject/klo-001.jp2"
-
-with Image.open(imageIn) as im:
-    im.load()
-    print(im.format)
