50ac59f45fb6b7e889a9ac71ca8bc53c226753d4
bitsgalore <johan.vanderknijff@kb.nl>
2024-11-08 18:11:18 +0100

    refactored stream property extraction to dedicated function

--- pdfquad/pdfquad.py
+++ pdfquad/pdfquad.py
@@ -403,7 +403,6 @@ def getProperties(PDF):
         images = page.get_images(full=False)
         for image in images:
             imageElt = etree.Element("image")
-            exceptionsStreamElt = etree.Element("exceptions")
             # Store properties at PDF object dictionary level to a dictionary
             propsDict = {}
             propsDict['xref'] = image[0]
@@ -416,74 +415,15 @@ def getProperties(PDF):
             #propsDict['name'] = image[7]
             propsDict['filter'] = image[8]
 
-            # Read raw image stream data from xref id
+            # Get raw image stream from xref           
             xref = propsDict['xref']
-            imageReadSuccess = False
-            propsStream = {}
             stream = doc.xref_stream_raw(xref)
-            try:
-                im = PIL.Image.open(io.BytesIO(stream))
-                im.load()
-                imageReadSuccess = True
-            except Exception as e:
-                ex = etree.SubElement(exceptionsStreamElt,'exception')
-                ex.text = str(e)
-                logging.warning(("page {} while reading image stream: {}").format(str(pageNo), str(e)))
 
-            if imageReadSuccess:
-                propsStream['format'] = im.format
-                width = im.size[0]
-                height = im.size[1]
-                propsStream['width'] = width
-                propsStream['height'] = height
-                propsStream['mode'] = im.mode
-                noComponents = len(im.getbands())
-                propsStream['components']= noComponents
-                bitsPerComponent = getBPC(im)
-                propsStream['bpc'] = bitsPerComponent
-
-                if im.format == "JPEG":
-                    try:
-                        # Estimate JPEG quality using least squares matching
-                        # against standard quantization tables
-                        quality, rmsError, nse = jpegquality.computeJPEGQuality(im)
-                        propsStream['JPEGQuality'] = quality
-                        propsStream['NSE_JPEGQuality'] = nse
-                    except Exception as e:
-                        ex = etree.SubElement(exceptionsStreamElt,'exception')
-                        ex.text = str(e)
-                        logging.warning(("page {} while estimating JPEG quality from image stream: {}").format(str(pageNo), str(e)))
-
-                for key, value in im.info.items():
-                    if isinstance(value, bytes):
-                        propsStream[key] = 'bytestream'
-                    elif key == 'dpi' and isinstance(value, tuple):
-                        propsStream['ppi_x'] = value[0]
-                        propsStream['ppi_y'] = value[1]
-                    elif key == 'jfif_density' and isinstance(value, tuple):
-                        propsStream['jfif_density_x'] = value[0]
-                        propsStream['jfif_density_y'] = value[1]
-                    elif isinstance(value, tuple):
-                        # Skip any other properties that return tuples
-                        pass
-                    else:
-                        propsStream[key] = value
-
-                try:
-                    # ICC profile name and description
-                    icc = im.info['icc_profile']
-                    iccProfile = ImageCms.ImageCmsProfile(io.BytesIO(icc))
-                    propsStream['icc_profile_name'] = ImageCms.getProfileName(iccProfile).strip()
-                    propsStream['icc_profile_description'] = ImageCms.getProfileDescription(iccProfile).strip()
-                except Exception as e:
-                    ex = etree.SubElement(exceptionsStreamElt,'exception')
-                    ex.text = str(e)
-                    logging.warning(("page {} while extracting ICC profile properties from image stream: {}").format(str(pageNo), str(e)))
+            # Extract stream properties
+            propsStreamElt = getImageStreamProperties(stream, pageNo)
 
             # Dictionaries to element objects
             propsDictElt = dictionaryToElt('dict', propsDict)
-            propsStreamElt = dictionaryToElt('stream', propsStream)
-            propsStreamElt.append(exceptionsStreamElt)
             # Add properties to image element
             imageElt.append(propsDictElt)
             imageElt.append(propsStreamElt)
@@ -505,6 +445,80 @@ def getProperties(PDF):
     propertiesElt.append(exceptionsFileElt)
 
     return propertiesElt
+
+
+def getImageStreamProperties(stream, pageNo):
+    """Extract image stream properties and return result as Element object"""
+
+    # Dictionary for storing stream properties
+    propsStream = {}
+    # Element for storing sttream-level exceptions
+    exceptionsStreamElt = etree.Element("exceptions")
+
+    try:
+        im = PIL.Image.open(io.BytesIO(stream))
+        im.load()
+    except Exception as e:
+        ex = etree.SubElement(exceptionsStreamElt,'exception')
+        ex.text = str(e)
+        propsStreamElt = dictionaryToElt('stream', propsStream)
+        propsStreamElt.append(exceptionsStreamElt)
+        logging.warning(("page {} while reading image stream: {}").format(str(pageNo), str(e)))
+        return propsStreamElt
+
+    propsStream['format'] = im.format
+    width = im.size[0]
+    height = im.size[1]
+    propsStream['width'] = width
+    propsStream['height'] = height
+    propsStream['mode'] = im.mode
+    noComponents = len(im.getbands())
+    propsStream['components']= noComponents
+    bitsPerComponent = getBPC(im)
+    propsStream['bpc'] = bitsPerComponent
+
+    if im.format == "JPEG":
+        try:
+            # Estimate JPEG quality using least squares matching
+            # against standard quantization tables
+            quality, rmsError, nse = jpegquality.computeJPEGQuality(im)
+            propsStream['JPEGQuality'] = quality
+            propsStream['NSE_JPEGQuality'] = nse
+        except Exception as e:
+            ex = etree.SubElement(exceptionsStreamElt,'exception')
+            ex.text = str(e)
+            logging.warning(("page {} while estimating JPEG quality from image stream: {}").format(str(pageNo), str(e)))
+
+    for key, value in im.info.items():
+        if isinstance(value, bytes):
+            propsStream[key] = 'bytestream'
+        elif key == 'dpi' and isinstance(value, tuple):
+            propsStream['ppi_x'] = value[0]
+            propsStream['ppi_y'] = value[1]
+        elif key == 'jfif_density' and isinstance(value, tuple):
+            propsStream['jfif_density_x'] = value[0]
+            propsStream['jfif_density_y'] = value[1]
+        elif isinstance(value, tuple):
+            # Skip any other properties that return tuples
+            pass
+        else:
+            propsStream[key] = value
+
+    try:
+        # ICC profile name and description
+        icc = im.info['icc_profile']
+        iccProfile = ImageCms.ImageCmsProfile(io.BytesIO(icc))
+        propsStream['icc_profile_name'] = ImageCms.getProfileName(iccProfile).strip()
+        propsStream['icc_profile_description'] = ImageCms.getProfileDescription(iccProfile).strip()
+    except Exception as e:
+        ex = etree.SubElement(exceptionsStreamElt,'exception')
+        ex.text = str(e)
+        logging.warning(("page {} while extracting ICC profile properties from image stream: {}").format(str(pageNo), str(e)))
+
+    propsStreamElt = dictionaryToElt('stream', propsStream)
+    propsStreamElt.append(exceptionsStreamElt)
+
+    return propsStreamElt
 
 
 def validate(schema, propertiesElt, verboseFlag):
