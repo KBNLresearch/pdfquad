47dd3e17d29c34530ac0af7cf8c6cadfa0f6d875
bitsgalore <johan.vanderknijff@kb.nl>
2024-11-08 19:57:13 +0100

    bumped version

--- pdfquad/pdfquad.py
+++ pdfquad/pdfquad.py
@@ -20,7 +20,7 @@ from . import properties
 from . import schematron
 from . import shared
 
-__version__ = "0.2.a6"
+__version__ = "0.2.a7"
 
 # Create parser
 parser = argparse.ArgumentParser(description="PDF QUality Assessment for Digitisation batches")
